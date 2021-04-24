244da4ef178e3833f74da5591f46fd1f4bd1b8d6
jawwad farooq <jawwad.ahmad@onebyte.biz>
2021-04-24 13:57:25 +0500

    sqlite integrated

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../providers/great_places.dart';
 import './add_place_screen.dart';
 import '../providers/great_places.dart';
 
@@ -19,25 +20,34 @@ class PlacesListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Consumer<GreatPlaces>(
-        child: Center(
-          child: const Text('Got no places yet, start adding some!'),
-        ),
-        builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
-            ? ch
-            : ListView.builder(
-                itemCount: greatPlaces.items.length,
-                itemBuilder: (ctx, i) => ListTile(
-                      leading: CircleAvatar(
-                        backgroundImage: FileImage(
-                          greatPlaces.items[i].image,
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(context, listen: false)
+            .fetchAndSetPlaces(),
+        builder: (ctx, response) => response.connectionState ==
+                ConnectionState.waiting
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Consumer<GreatPlaces>(
+                child: Center(
+                  child: const Text('Got no places yet, start adding some!'),
+                ),
+                builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+                    ? ch
+                    : ListView.builder(
+                        itemCount: greatPlaces.items.length,
+                        itemBuilder: (ctx, i) => ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: FileImage(
+                              greatPlaces.items[i].image,
+                            ),
+                          ),
+                          title: Text(greatPlaces.items[i].title),
+                          onTap: () {
+                            // Go to detail page ...
+                          },
                         ),
                       ),
-                      title: Text(greatPlaces.items[i].title),
-                      onTap: () {
-                        // Go to detail page ...
-                      },
-                    ),
               ),
       ),
     );
